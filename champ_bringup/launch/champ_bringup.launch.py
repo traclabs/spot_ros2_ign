3fe0777c3baad80e59c865d5a728a1ff74550b2b
Ana C. Huaman Quispe <ana@traclabs.com>
2025-03-21 10:03:45 -0500

    Spot shows up in Gazebo

--- champ_bringup/launch/champ_bringup.launch.py
+++ champ_bringup/launch/champ_bringup.launch.py
@@ -13,269 +13,192 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node, SetParameter
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
 from launch.actions import RegisterEventHandler
 from launch.actions import IncludeLaunchDescription
 from launch.actions import ExecuteProcess
 from launch.event_handlers import OnProcessExit
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import Command
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch.substitutions import PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
+from os import environ
 import os
 import xacro
 
-# This will only take in effect if you are running in Simulation
-os.environ['GAZEBO_MODEL_PATH'] = os.path.join(get_package_share_directory('champ_gazebo'),
-                                               'models')
-
-
 def generate_launch_description():
 
-    # Get here directories of packages
-    champ_bringup_share_dir = get_package_share_directory(
-        'champ_bringup')
-    champ_description_share_dir = get_package_share_directory(
-        'champ_description')
+  # Get here directories of packages
+  champ_bringup_share_dir = get_package_share_directory(
+      'champ_bringup')
+  champ_description_share_dir = get_package_share_directory(
+      'champ_description')
 
-    use_simulator = LaunchConfiguration('use_simulator')
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-    use_rviz = LaunchConfiguration("use_rviz")
-    tf_prefix = LaunchConfiguration('tf_prefix')
-    rviz_config = LaunchConfiguration("rviz_config")
-    champ_params = LaunchConfiguration('champ_params')
-
-    declare_use_simulator = DeclareLaunchArgument(
+  launch_args = [
+    DeclareLaunchArgument(
         'use_simulator',
         default_value='True',
-        description='whether to use Gazebo Simulation.')
-    declare_use_sim_time = DeclareLaunchArgument(
+        description='whether to use Gazebo Simulation.'),
+    DeclareLaunchArgument(
         'use_sim_time',
         default_value='True',
-        description='If true, use simulated clock')
-    declare_use_rviz = DeclareLaunchArgument(
+        description='If true, use simulated clock'),
+    DeclareLaunchArgument(
         'use_rviz',
         default_value='False',
-        description='whether or not to launch rviz')
-    declare_tf_prefix = DeclareLaunchArgument(
+        description='whether or not to launch rviz'),
+    DeclareLaunchArgument(
         'tf_prefix',
         default_value='',
-        description='...')
-    declare_rviz_config = DeclareLaunchArgument(
+        description='...'),
+    DeclareLaunchArgument(
         'rviz_config',
         default_value=os.path.join(
             champ_bringup_share_dir, 'rviz', 'default_view.rviz'),
-        description='...')
-    declare_champ_params = DeclareLaunchArgument(
+        description='...'),
+    DeclareLaunchArgument(
         'champ_params',
         default_value=os.path.join(
             champ_bringup_share_dir, 'config', 'champ_params.yaml'),
-        description='path to locks params.')
-
-    # DECLARE THE msg relay ROS2 NODE
-    declare_quadruped_controller_node = Node(
-        package='champ_base',
-        executable='quadruped_controller',
-        # name='quadruped_controller',
-        output='screen',
-        # namespace='',
-        arguments=['--ros-args', '--log-level', 'INFO'],
-        # prefix=['xterm -e gdb -ex run --args'],
-        
-        parameters=[
-            {"use_sim_time": use_sim_time},
-            champ_params],
-        remappings=[('cmd_vel', 'vox_nav/cmd_vel')]
-        )
-
-    # DECLARE THE msg relay ROS2 NODE
-    declare_state_estimation_node = Node(
-        package='champ_base',
-        executable='state_estimation',
-        # name='state_estimation',
-        output='screen',
-        # namespace='',
-        parameters=[
-            {"use_sim_time": use_sim_time},
-            champ_params],
-        remappings=[('cmd_vel', 'vox_nav/cmd_vel')]
-        #prefix=['xterm -e gdb -ex run --args'],
-    )
-
-    #xacro_file_name = 'champ/champ.urdf.xacro'
-    xacro_file_name = 'spot/spot.urdf.xacro'
-
-    xacro_full_dir = os.path.join(
-        champ_description_share_dir, 'urdf', xacro_file_name)
-
-    print(xacro_full_dir)
-
-    robot_description = xacro.process_file( 
-       xacro_full_dir,
-       mappings={"simulate_cameras": "True",
-                 "visualize": "False"}).toprettyxml(indent="  ")
-
-    declare_robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{"use_sim_time": use_sim_time},
-                    {'robot_description': robot_description}],
-        remappings=[('/tf', 'tf'),
-                    ('/tf_static', 'tf_static')])
-
-    # Spawn the robot in Ignition Gazebo
-    declare_spawn_entity_to_gazebo_node = Node(
-        package='ros_gz_sim',
-        executable='create',
-        condition=IfCondition(use_simulator),
-        arguments=['-name', 'spot',
-                   '-topic', '/robot_description', "-z", "0.84"],
-        output='screen',
-        parameters=[{"use_sim_time": use_sim_time}])
-
-    # Start Ignition Gazebo
-    # gazebo_world = os.path.join(get_package_share_directory('champ_gazebo'), 'worlds/', 'ground_plane.sdf')
-    # declare_start_gazebo_cmd = ExecuteProcess(
-    #     cmd=['ign', 'gazebo', gazebo_world],
-    #     condition=IfCondition(PythonExpression([use_simulator])),
-    #     output='screen')  
-
-
-    #  INCLUDE RVIZ LAUNCH FILE IF use_rviz IS SET TO TRUE
-    declare_rviz_launch_include = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        os.path.join(champ_bringup_share_dir,
-                     'launch',
-                     'rviz.launch.py')),
-        condition=IfCondition(use_rviz),
-        launch_arguments={
-        'rviz_config': rviz_config
-    }.items())
-
-    localization_params = LaunchConfiguration('localization_params')
-    declare_localization_params = DeclareLaunchArgument(
+        description='path to locks params.'),
+    DeclareLaunchArgument(
         'localization_params',
         default_value=os.path.join(
             champ_bringup_share_dir, 'config', 'robot_localization_params.yaml'),
         description='Path to the vox_nav parameters file.')
-
-    base_to_footprint_ekf = Node(package='robot_localization',
-                                 executable='ekf_node',
-                                 name='base_to_footprint_ekf',
-                                 output='screen',
-                                 parameters=[localization_params, {"use_sim_time" : True}],
-                                 remappings=[('odometry/filtered', 'odometry/local')])
-
-    footprint_to_odom_ekf = Node(package='robot_localization',
-                                 executable='ekf_node',
-                                 name='footprint_to_odom_ekf',
-                                 output='screen',
-                                 parameters=[localization_params, {"use_sim_time" : True}],
-                                 remappings=[('odometry/filtered', 'odom')])
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_trajectory_controller'],
-        output='screen'
-    )
-
-    #bridge = Node(
-    #    package='ros_gz_bridge',
-    #    executable='parameter_bridge',
-    #    arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-    #    output='screen'
-    #)
-
-    bridge_config_file = os.path.join(
-        champ_bringup_share_dir, 'config', "spot_bridge.yaml")
-
-
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{'config_file': bridge_config_file}],
-        #arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
-    )
-
-
-    #joint_state_publisher_gui = Node(
-    #    package='joint_state_publisher_gui',
-    #    executable='joint_state_publisher_gui',
-    #    name='joint_state_publisher_gui',
-    #    output='screen',
-    #    parameters=[{'use_sim_time': True}]
-    #)
-
-    # Running tf_broadcaster
-    broadcast_left_front_cam = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        #parameters=[{'config_file': bridge_config_file}],
-        arguments=["--x", "0", "--y", "0", "--z", "0",
-        "--roll", "0", "--pitch", "0", "--yaw", "0",
-        "--frame-id", "camera_frontleft", "--child-frame-id", "spot/camera_frontleft/frontleft_depth"],
-        output='screen'
-    )
-
-    broadcast_right_front_cam = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        #parameters=[{'config_file': bridge_config_file}],
-        arguments=["--x", "0", "--y", "0", "--z", "0",
-        "--roll", "0", "--pitch", "0", "--yaw", "0",
-        "--frame-id", "camera_frontright", "--child-frame-id", "spot/camera_frontright/frontright_depth"],
-        output='screen'
-    )
+  ]
+  
+  use_simulator = LaunchConfiguration('use_simulator')
+  use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+  use_rviz = LaunchConfiguration("use_rviz")
+  tf_prefix = LaunchConfiguration('tf_prefix')
+  rviz_config = LaunchConfiguration("rviz_config")
+  champ_params = LaunchConfiguration('champ_params')  
     
+  # Gazebo setup
+  champ_models_path = get_package_share_directory('champ_gazebo')
+    
+  sim_resource_path = os.pathsep.join(
+            [
+                environ.get("GZ_SIM_RESOURCE_PATH", default=""),
+                os.path.join( champ_models_path, 'models' )
+            ]
+  )
+  env_gz_sim = SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', sim_resource_path)
 
-    ground_plane_path=os.path.join(get_package_share_directory('champ_gazebo'), 'worlds', 'ground_plane.sdf')
-    marsyard_path=os.path.join(get_package_share_directory('champ_gazebo'), 'worlds', 'marsyard2020.sdf')
 
-    return LaunchDescription([
-        bridge,
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory('ros_ign_gazebo'),
-                              'launch', 'ign_gazebo.launch.py')]),
-            launch_arguments=[('gz_args', [' -r -v 4 ', marsyard_path])]),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=declare_spawn_entity_to_gazebo_node,
-                on_exit=[load_joint_state_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_joint_state_controller,
-                on_exit=[load_joint_trajectory_controller],
-            )
-        ),
-        declare_use_simulator,
-        declare_use_rviz,
-        declare_tf_prefix,
-        declare_rviz_config,
-        declare_champ_params,
-        declare_quadruped_controller_node,
-        declare_state_estimation_node,
-        declare_robot_state_publisher_node,
-        declare_rviz_launch_include,
-        declare_spawn_entity_to_gazebo_node,
-        declare_localization_params,
-        footprint_to_odom_ekf,
-        base_to_footprint_ekf,
-        declare_use_sim_time,
-        broadcast_left_front_cam,
-        broadcast_right_front_cam
-        #joint_state_publisher_gui  # added by diya 9/6
+  # Start Gazebo
+  ground_plane_sdf=PathJoinSubstitution([FindPackageShare('champ_gazebo'), 'worlds', 'ground_plane.sdf'])
+  marsyard_sdf=PathJoinSubstitution([FindPackageShare('champ_gazebo'), 'worlds', 'marsyard2020.sdf'])
+  gz_launch = IncludeLaunchDescription(
+            PathJoinSubstitution([FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py']),
+            launch_arguments = [
+               ('gz_args', [
+                   marsyard_sdf,
+                   ' -r',
+                   ' -v 4' 
+               ])
+            ],
+            condition=IfCondition(use_simulator)
+  )
+
+  # Bridge
+  bridge_config_file = os.path.join(
+      champ_bringup_share_dir, 'config', "spot_bridge.yaml")
+
+  bridge = Node(
+      package='ros_gz_bridge',
+      executable='parameter_bridge',
+      parameters=[{'config_file': bridge_config_file}],
+      #arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+      output='screen'
+  )
+
+  # Robot spawn/publisher
+  xacro_file_name = 'spot/spot.urdf.xacro' #'champ/champ.urdf.xacro'
+
+  xacro_full_dir = os.path.join(
+      champ_description_share_dir, 'urdf', xacro_file_name)
+
+  print(xacro_full_dir)
+
+  robot_description = xacro.process_file( 
+     xacro_full_dir,
+     mappings={"simulate_cameras": "True",
+               "visualize": "False"}).toprettyxml(indent="  ")
+
+  robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[{"use_sim_time": use_sim_time},
+                    {'robot_description': robot_description}],
+        remappings=[('/tf', 'tf'),
+                    ('/tf_static', 'tf_static')],
+        output='screen'
+  )
+
+
+  # Spawn the robot in Ignition Gazebo
+  spawn_entity_to_gazebo_node = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-name', 'spot',
+                   '-topic', '/robot_description', "-z", "0.0"], # 0.84
+        parameters=[{"use_sim_time": use_sim_time}],
+        output='screen',
+        condition=IfCondition(use_simulator)        
+  )
+
+  load_joint_state_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        name="start_joint_state_broadcaster",
+        output='screen'
+  )
+
+
+  load_joint_trajectory_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+        name="start_joint_trajectory_controller",
+        output='screen',
+  )
+
+    
+  return LaunchDescription(
+    launch_args + 
+    [
+      SetParameter(name='use_sim_time', value=True), 
+      env_gz_sim,
+      bridge,
+      gz_launch,
+      robot_state_publisher_node,
+      spawn_entity_to_gazebo_node,      
+      RegisterEventHandler(
+          event_handler=OnProcessExit(
+              target_action=spawn_entity_to_gazebo_node,
+              on_exit=[load_joint_state_controller],
+          )
+      ),
+      RegisterEventHandler(
+          event_handler=OnProcessExit(
+              target_action=load_joint_state_controller,
+              on_exit=[load_joint_trajectory_controller],
+          )
+      ),
+      #declare_quadruped_controller_node,
+      #declare_state_estimation_node,
+      #declare_rviz_launch_include,
+      #declare_localization_params,
+      #footprint_to_odom_ekf,
+      #base_to_footprint_ekf,
+      #broadcast_left_front_cam,
+      #broadcast_right_front_cam
+      #joint_state_publisher_gui  # added by diya 9/6
     ])
